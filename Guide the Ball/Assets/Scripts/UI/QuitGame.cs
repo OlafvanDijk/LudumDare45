d80d475edfc2a41c7301f11817e71243993df428
OlafvanDijk <o.olaf@hotmail.com>
2019-10-06 12:15:54 +0200

    Scaled UI and added Quit option

---
+++ Guide the Ball/Assets/Scripts/UI/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
